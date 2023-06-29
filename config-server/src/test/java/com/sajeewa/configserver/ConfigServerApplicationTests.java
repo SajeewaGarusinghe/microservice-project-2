6ce02324a17eb4d8b2839d328652160e1469df74
sajeewa garusinghe <sajeeva616@gmail.com>
2023-06-29 11:54:13 +0530

    apigateway,discovery server and config server created

---
+++ config-server/src/test/java/com/sajeewa/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajeewa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
