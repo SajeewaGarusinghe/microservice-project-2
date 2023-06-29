a661154680c48417a4910f4598729f43128509df
sajeewa garusinghe <sajeeva616@gmail.com>
2023-06-29 22:45:11 +0530

    configuration server working fine and zipkin added and working well

--- school/src/main/java/com/sajeewa/school/repo/SchoolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sajeewa.school.repo;
-
-import com.sajeewa.school.entity.School;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School, Integer> {
-}
